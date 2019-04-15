16da71aecfbf4dc8b12e1c23a5f1edc2119369dd
Greg Williams <greggwilliams58@hotmail.com>
2019-04-15 10:41:12 +0100

    first attempts with datetime
    interrupted with fares index work

--- RME_Rail_Fares/NRE webscrape.py
+++ RME_Rail_Fares/NRE webscrape.py
@@ -2,48 +2,66 @@ import urllib.request
 from bs4 import BeautifulSoup
 import json
 import pprint as pp
-
+from datetime import datetime, timedelta
 
 
 
 def main():
 
-    response = urllib.request.urlopen(f"http://ojp.nationalrail.co.uk/service/timesandfares/KGX/EDB/120419/1830/dep/150419/1200/dep")
+    todaydate = setdates()
+
+    
+    response = urllib.request.urlopen(f"http://ojp.nationalrail.co.uk/service/timesandfares/KGX/EDB/200419/1830/dep/220419/1200/dep")
     soup = BeautifulSoup(response,'html.parser')
 
-    datapackage = list()
+    #datapackage = list()
 
-    #print(soup)
+    ##print(soup)
 
-    with open("Full text from NRE.txt","w") as f:
-        f.write(soup.text)
+    #with open("Full text from NRE.txt","w") as f:
+    #    f.write(soup.text)
 
-    for i in range (1,6):
-        td_class = soup.find('script',{ 'id':f'jsonJourney-4-{i}' }).text
+    #for i in range (1,6):
+    #    td_class = soup.find('script',{ 'id':f'jsonJourney-4-{i}' }).text
         
-        jsonData = json.loads(td_class)
-        datapackage.append(jsonData)
+    #    jsonData = json.loads(td_class)
+    #    datapackage.append(jsonData)
     
-    for item in datapackage:
-        pp.pprint(item)
-        print("new record \n")
+    #for item in datapackage:
+    #    pp.pprint(item)
+    #    print("new record \n")
         
     
-    #print(datapackage[0]['jsonJourneyBreakdown']['arrivalStationName'])  
-    #print(datapackage[0]['jsonJourneyBreakdown']['departureStationName'])
+    ##print(datapackage[0]['jsonJourneyBreakdown']['arrivalStationName'])  
+    ##print(datapackage[0]['jsonJourneyBreakdown']['departureStationName'])
     
-    #for counter,items in enumerate(datapackage):
-       ##pp.pprint(F"This is counter {counter}")
-       ##pp.pprint(f"This is items {items}")
-       ##print(type(counter))
-       ##print(type(items))
+    ##for counter,items in enumerate(datapackage):
+    #   ##pp.pprint(F"This is counter {counter}")
+    #   ##pp.pprint(f"This is items {items}")
+    #   ##print(type(counter))
+    #   ##print(type(items))
 
 
-    #    pp.pprint(items.get('singleJsonFareBreakdowns','no')[0])
-    #    pp.pprint(resp.get('fares','no dest')[counter].get('adult','no code').get('fare','no fares'))
-    #    print(items['singleJsonFareBreakdowns'][counter]['nreFareCategory'])
-    #    print(items['singleJsonFareBreakdowns'][counter]['fullFarePrice'])
+    ##    pp.pprint(items.get('singleJsonFareBreakdowns','no')[0])
+    ##    pp.pprint(resp.get('fares','no dest')[counter].get('adult','no code').get('fare','no fares'))
+    ##    print(items['singleJsonFareBreakdowns'][counter]['nreFareCategory'])
+    ##    print(items['singleJsonFareBreakdowns'][counter]['fullFarePrice'])
+   
+def setdates():
+    todaysdate = datetime.today()
+    #dayahead = datetime.today() + timedelta(todaysdate=1)
+    #weekahead = todaysdate + timedelta(days=7)
+    #monthahead = todaysdate +timedelta(month=1)
+
+
+    currentdate = dayahead.strftime('%d%m%y')
+    print(f"This is the day ahead {currentdate}")
+
+
     
+
+
+    return currentdate
 
 
 
