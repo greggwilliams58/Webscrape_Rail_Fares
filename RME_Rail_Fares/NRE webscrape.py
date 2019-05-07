6060a8ab085bb3f50e74e6553bdc5ca26c38ac13
Greg Williams <greggwilliams58@hotmail.com>
2019-05-07 16:44:22 +0100

    some progress made
    1) Full, correct list of URLs generated
    2) BS extracts correct data
    3) data parsed as json and extracted as descrete variables
    
    BUT conversion of urlprocess as returning a dictionary has screwed it up

--- RME_Rail_Fares/NRE webscrape.py
+++ RME_Rail_Fares/NRE webscrape.py
@@ -9,81 +9,110 @@ from collections import defaultdict
 
 def main():
 
-    todaydate = setdates()
-    #print(todaydate)
-    #pp.pprint(todaydate)
-    #print(todaydate.values())
+    collecteddata = collatedata()
+
+    #print(collecteddata)
+    #test URLS
+    #urlstoprocess =  {'140519':'http://ojp.nationalrail.co.uk/service/timesandfares/KGX/EDB/140519/1430/dep','150519':'http://ojp.nationalrail.co.uk/service/timesandfares/KGX/EDB/150519/1430/dep'}
+
+    # the real process here
+    #full list of URLs to be generated
+    urlstoprocess = generateurl(collecteddata)
+    print(type(urlstoprocess))
+
+    for k,v in urlstoprocess.values():
+        pp.pprint( f":{k} {v}")
+#        for times in v:
+#            pp.pprint(times)
+
+    print("getting url info")
+
+    rawjsondata = list()
     
+    
+    #for k,v in urlstoprocess.items():
 
-    #for date,times in todaydate.items():
-    #   print(date ,times)
-      
+    #    response = urllib.request.urlopen(v)
+    #    soup = BeautifulSoup(response,'html.parser')
 
-    originanddestination = [['KGX','EDB'],['EDB','KGX']]
-
-
-
-    for routes in originanddestination:
-        print(routes)
-        for dayoftravel in todaydate.keys():
-            for counter,_ in enumerate(dayoftravel,0):
-                #for some reason this only prints 6 of each - 
-                #I suspect the wrong object is being iterated over (dates rather than the list of times)
-                if routes[0] ==  'KGX':
-                    url = 'http://ojp.nationalrail.co.uk/service/timesandfares/'+routes[0]+'/'+routes[1]+'/'+dayoftravel+'/'+str(todaydate[dayoftravel][0][counter])+'/dep/'
-                    print(url)
-
-                elif routes[0] == 'EDB':
-                    url = 'http://ojp.nationalrail.co.uk/service/timesandfares/'+routes[0]+'/'+routes[1]+'/'+dayoftravel+'/'+str(todaydate[dayoftravel][1][counter])+'/dep/'
-                    print(url)
-
-
-   
- 
-    #response = urllib.request.urlopen(f"http://ojp.nationalrail.co.uk/service/timesandfares/KGX/EDB/290519/1830/dep/")
-    #soup = BeautifulSoup(response,'html.parser')
-
-    ##datapackage = list()
-
-    #print(soup)
-
-    ##with open("Full text from NRE.txt","w") as f:
-    ##    f.write(soup.text)
-
-    #for i in range (1,6):
     #    td_class = soup.find('script',{ 'id':f'jsonJourney-4-1' }).text
-    #    
-    #jsonData = json.loads(td_class)
-    #pp.pprint(jsonData)
+
+   #     jsonData = json.loads(td_class)
+   #     jsonData['jsonJourneyBreakdown'].update(TravelDate = k)
+
+   #     rawjsondata.append(jsonData)
+
+   # answer = processjson(rawjsondata)
+
+
+
+def processjson(jsoninfo):
+    for journey in jsoninfo:
+        print(type(journey))
+        print (f"travel date \t {journey['jsonJourneyBreakdown']['TravelDate']}")
+        print (f"arrival \t {journey['jsonJourneyBreakdown']['arrivalStationName']}")
+        print(f"departure \t{journey['jsonJourneyBreakdown']['departureStationName']}")
+        print(f"time \t{journey['jsonJourneyBreakdown']['departureTime']}")
+        print(f"type \t{journey['singleJsonFareBreakdowns'][0]['breakdownType']}")
+        print(f"1st class \t{journey['singleJsonFareBreakdowns'][0]['cheapestFirstClassFare']}")
+        print(f"discount \t{journey['singleJsonFareBreakdowns'][0]['discount']}")
+        print(f"fareprovider \t{journey['singleJsonFareBreakdowns'][0]['fareProvider']}")
+        print(f"route \t{journey['singleJsonFareBreakdowns'][0]['fareRouteDescription']}")
+        print(f"route name\t{journey['singleJsonFareBreakdowns'][0]['fareRouteName']}")
+        print(f"ticket type\t fullfare{journey['singleJsonFareBreakdowns'][0]['fareTicketType']}")
+        print(f"full fare \t{journey['singleJsonFareBreakdowns'][0]['fullFarePrice']}")
+        print(f"nre category \t{journey['singleJsonFareBreakdowns'][0]['nreFareCategory']}")
+        print(f"ticket price \t{journey['singleJsonFareBreakdowns'][0]['ticketPrice']}")
+
+
+
+
+
+def generateurl(collecteddata):
+    urltoprocess = defaultdict(list)
+    tempurldown = list()
+    tempurlup = list()
+
+    dateoftravel = list(collecteddata.keys())
+
+    for date in dateoftravel:
+        #print(date)
+        for data in collecteddata[date]:
+            if data[0][0] ==  'KGX':
+                for counter,downtime in enumerate(data[1],0):
+                    url = 'http://ojp.nationalrail.co.uk/service/timesandfares/'+data[0][0]+'/'+data[0][1]+'/'+date+'/'+str(data[1][counter])+'/dep/'
+                    
+                    
+                    
+                
+            if data[2][0] == 'EDB':
+                for counter,updtime in enumerate(data[3],0):
+                    url = 'http://ojp.nationalrail.co.uk/service/timesandfares/'+data[2][0]+'/'+data[2][1]+'/'+date+'/'+str(data[3][counter])+'/dep/'
+                    tempurlup.append(url)
+    urltoprocess[date] = [tempurldown,tempurlup]               
+    print(date)
     
-      
+    #pp.pprint(tempurldown)
+    #pp.pprint(tempurlup)
+    #urltoprocess[date] = [tempurldown,tempurlup]
     
-    ##print(datapackage[0]['jsonJourneyBreakdown']['arrivalStationName'])  
-    ##print(datapackage[0]['jsonJourneyBreakdown']['departureStationName'])
-    
-    ##for counter,items in enumerate(datapackage):
-    #   ##pp.pprint(F"This is counter {counter}")
-    #   ##pp.pprint(f"This is items {items}")
-    #   ##print(type(counter))
-    #   ##print(type(items))
+    #for k,v in urltoprocess.items():
+    #    print(k)
+    #    print(v)
 
-
-    ##    pp.pprint(items.get('singleJsonFareBreakdowns','no')[0])
-    ##    pp.pprint(resp.get('fares','no dest')[counter].get('adult','no code').get('fare','no fares'))
-    ##    print(items['singleJsonFareBreakdowns'][counter]['nreFareCategory'])
-    ##    print(items['singleJsonFareBreakdowns'][counter]['fullFarePrice'])
-
-
+    return urltoprocess
+        
 
    
-def setdates(basedate = datetime.today()+timedelta(days=3)):
-    
+def collatedata(basedate = datetime.today()+timedelta(days=2)):
+  
 
     weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
 
     datesandtimes = defaultdict(list)
     
-    
+    originanddestination = [['KGX','EDB'],['EDB','KGX']]
+
     downweekdaytimes = ['0612','0900','1000','1030','1200','1300','1330','1500','1530','1400','1900']
     downsaturdaytimes = ['0612','0830','0900','0930','1130','1200','1230','1430','1500','1530','1800']
     downsundaytimes = ['0848','0900','0930','1030','1100','1122','1230','1300','1330','1530','1600','1630','1900']
@@ -92,7 +121,7 @@ def setdates(basedate = datetime.today()+timedelta(days=3)):
     upsaturdaytimes = ['0626','0655','0930','1000','1130','1200','1330','1400','1630','1700']
     upsundaytimes = ['1030','1100','1430','1450','1630','1700','1800','1830']
 
-    #datetocheck = datetime.today()+timedelta(days=3)
+    datetocheck = datetime.today()+timedelta(days=1)
    
     daystomoveahead = [1,7,30]
     datestocheck = []
@@ -119,32 +148,10 @@ def setdates(basedate = datetime.today()+timedelta(days=3)):
         else:
             print("error")
 
-        datesandtimes[formattedfuturedate] = [downtimestocheck,uptimestocheck]
-
-    #print(type(datesandtimes))
-    #pp.pprint(datesandtimes)
-
-
+        datesandtimes[formattedfuturedate] = [[originanddestination[0],downtimestocheck,originanddestination[1],uptimestocheck]]
 
     return datesandtimes
 
-
-#def settimes():
-#    """
-#    A collection of lists that define the departure times to be searched for
-#    down = away from London
-#    up = towards London
-#    """
-#
-#    downweekdaytimes = ['0612','0900','1000','1030','1200','1300','1330','1400','1500','1530','1900']
-#    downsaturdaytimes = ['0612','0830','0900','0930','1130','1200','1230','1430','1500','1530','1800']
-#    downsundaytimes = ['0848','0900','0930','1030','1100','1122','1230','1300','1330','1530','1600','1630','1900']
-#
-#    upweekdaytimes = ['0656','0830','0900','1030','1130','1400','1430','1530','1731','1830','1936']
-#    upsaturdaytimes = ['0626','0655','0930','1000','1130','1200','1330','1400','1630','1700']
-#    upsundaytimes = ['1030','1100','1430','1450','1630','1700','1800','1830']#
-#
-#    return 
 
 
 if __name__ == '__main__':
