50360046790dd9e64161428ca9c10a8c283f85a9
Greg Williams <greggwilliams58@hotmail.com>
2019-06-05 20:05:02 +0100

    attempt to get multiple route, dates and direction via excel
    Excel data extraction of parameters works; but the results are not correct; need correct permutation of values to run through array.

--- RME_Rail_Fares/NRE webscrape.py
+++ RME_Rail_Fares/NRE webscrape.py
@@ -5,17 +5,23 @@ import pprint as pp
 from datetime import datetime, timedelta
 from collections import defaultdict
 import csv
+import pandas as pd
 
 
 def main():
     formatted_date = datetime.now().strftime('%Y%m%d_%H-%M')
 
+    routesandtimedatafp = 'C:\\Users\\gwilliams\\Documents\\GitHub\\RME_Rail_Fares\\datafile.xlsx'
     filepath = 'C:\\Users\\gwilliams\\Desktop\\Python Experiments\\work projects\\RME_Rail_Fares\\'
     filename = f'RME_data_{formatted_date}.csv'
 
+    alltimesdates = gettingquerydata(routesandtimedatafp)
+    #pp.pprint(alltimesdates)
+    
     #generated the sets of dates and times to work with
-    collateddatesandtime = getdatetimesinfo()
+    collateddatesandtime = getdatetimesinfo(alltimesdates)
 
+    #pp.pprint(collateddatesandtime)
     #full list of URLs to be generated
     urlstoprocess = generateurl(collateddatesandtime)
     
@@ -157,21 +163,33 @@ def generateurl(collecteddateinfo):
     tempurlup = []
 
     #extract dates of travel from keys of dictionary
-    dateoftravel = list(collecteddateinfo.keys())
-
+    routesoftravel = list(collecteddateinfo.keys())
+    #pp.pprint(routesoftravel)
     #walk through dates, routes and times to create url
-    for date in dateoftravel:
-        
-        for data in collecteddateinfo[date]:
-            if data[0][0] ==  'KGX':
-                for counter,downtime in enumerate(data[1],0):
-                    url = [date,'http://ojp.nationalrail.co.uk/service/timesandfares/'+data[0][0]+'/'+data[0][1]+'/'+date+'/'+str(data[1][counter])+'/dep/']
+    #pp.pprint(dateoftravel)
+
+
+    for routeanddate in routesoftravel:
+        #pp.pprint(routeanddate)
+        for counter,data in enumerate(collecteddateinfo[routeanddate]):
+            #pp.pprint(data)
+            #print(f"This is origins: {routeanddate[3:6]} and {data[1][0]}")
+            #print(f"This is destination: {route[3:6]} and {data[1][1]}")
+
+            if routeanddate[:3] ==  data[1][0] :
+                #print(f"this is down route extract {routeanddate[:3]} and this is the data extract {data[1][0]}")
+                for counter,downtime in enumerate(data[2],0):
+                    #print(counter)
+                    url = [data[0],'http://ojp.nationalrail.co.uk/service/timesandfares/'+data[1][0]+'/'+data[1][1]+'/'+data[0]+'/'+str(data[2][counter])+'/dep/']
+                    print(url)
                     tempurldown.append(url)
 
                     
-            if data[2][0] == 'EDB':
-                for counter,updtime in enumerate(data[3],0):
-                    url = [date,'http://ojp.nationalrail.co.uk/service/timesandfares/'+data[2][0]+'/'+data[2][1]+'/'+date+'/'+str(data[3][counter])+'/dep/']
+            if routeanddate[3:6] != data[1][0]:
+                #print(f"this is up route extract {routeanddate[3:6]} and this is the data extract {data[1][1]}")
+                for counter,uptime in enumerate(data[3],0):
+                    url = [data[0],'http://ojp.nationalrail.co.uk/service/timesandfares/'+data[3][0]+'/'+data[3][1]+'/'+data[0]+'/'+str(data[4][counter])+'/dep/']
+                    print(url)
                     tempurlup.append(url)
 
     #combine both up and down routes into a new common list
@@ -180,7 +198,7 @@ def generateurl(collecteddateinfo):
     return urltoprocess
         
   
-def getdatetimesinfo(dateoffset = 0):
+def getdatetimesinfo(routesandtimes, dateoffset = 0):
     """
     This is an 'initialisation' procedure which sets most of the parameters for the functioning of the whole process
     It takes a date and derives the dates 1,7 and 30 days in the future and then dertives the appropriate days, origin and destination routes and departure times for each of these factors
@@ -192,57 +210,121 @@ def getdatetimesinfo(dateoffset = 0):
     datesandtimes:  A default dictionary containing {dateoftravel:[[up journey],[times],[down journey],[times]]}
 
     """
+    #pp.pprint(routesandtimes)
+    
     #increment date to check if needed
     datetocheck = datetime.today()+timedelta(days=dateoffset)
     
     #initialisation information
     weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-       
-    originanddestination = [['KGX','EDB'],['EDB','KGX']]
-
-    downweekdaytimes = ['0612','0900','1000','1030','1200','1300','1330','1500','1530','1400','1900']
-    downsaturdaytimes = ['0612','0830','0900','0930','1130','1200','1230','1430','1500','1530','1800']
-    downsundaytimes = ['0848','0900','0930','1030','1100','1122','1230','1300','1330','1530','1600','1630','1900']
-
-    upweekdaytimes = ['0656','0830','0900','1030','1130','1400','1430','1530','1731','1830','1936']
-    upsaturdaytimes = ['0626','0655','0930','1000','1130','1200','1330','1400','1630','1700']
-    upsundaytimes = ['1030','1100','1430','1450','1630','1700','1800','1830']
-
-    daystomoveahead = [1,7,30]
     
+    daystomoveahead = [1,7,30]
     #populate defaultdict with values depending on date increments day of the week
+    
     datesandtimes = defaultdict(list)
     
-    #for each date to move ahead increment
-    for counter,item in enumerate(daystomoveahead):
-        futuredate = datetocheck + timedelta(daystomoveahead[counter])
+    ###start loop with all the dates and times here
+    for count, items in enumerate(routesandtimes):
+        originanddestination = routesandtimes[count][0]
+      
+        downweekdaytimes = routesandtimes[count][1]
+        downsaturdaytimes = routesandtimes[count][2]
+        downsundaytimes = routesandtimes[count][3]
 
-        formattedfuturedate, dayofweek = futuredate.strftime('%d%m%y'), weekdays[futuredate.weekday()]
+        upweekdaytimes = routesandtimes[count][4]
+        upsaturdaytimes = routesandtimes[count][5]
+        upsundaytimes = routesandtimes[count][6]
+        
+        startpoint = routesandtimes[count][0][0][0]
+        endpoint = routesandtimes[count][0][0][1]
 
-        #derive day of week from date
-        daycheck = weekdays[futuredate.weekday()]
-        if daycheck in ("Monday","Tuesday","Wednesday","Thursday","Friday"):
-            daytocheck = 'weekday'
-            downtimestocheck = downweekdaytimes
-            uptimestocheck = upweekdaytimes
-        elif daycheck in ("Saturday"):
-            daytocheck = 'saturday'
-            downtimestocheck = downsaturdaytimes
-            uptimestocheck = upsaturdaytimes
-        elif daycheck in ("Sunday"):
-            daytocheck = 'sunday'
-            downtimestocheck = downsundaytimes
-            uptimestocheck = upsundaytimes
-        else:
-            print("error")
+        for upanddown in routesandtimes[count][0]:
+            print(upanddown[0])
+        #for each date to move ahead increment
+            for counter,item in enumerate(daystomoveahead):
 
-        datesandtimes[formattedfuturedate] = [[originanddestination[0],downtimestocheck,originanddestination[1],uptimestocheck]]
+                futuredate = datetocheck + timedelta(daystomoveahead[counter])
 
+                formattedfuturedate, dayofweek = futuredate.strftime('%d%m%y'), weekdays[futuredate.weekday()]
+
+
+                #derive day of week from date
+                daycheck = weekdays[futuredate.weekday()]
+                if daycheck in ("Monday","Tuesday","Wednesday","Thursday","Friday"):
+                    daytocheck = 'weekday'
+                    downtimestocheck = downweekdaytimes
+                    uptimestocheck = upweekdaytimes
+                elif daycheck in ("Saturday"):
+                    daytocheck = 'saturday'
+                    downtimestocheck = downsaturdaytimes
+                    uptimestocheck = upsaturdaytimes
+                elif daycheck in ("Sunday"):
+                    daytocheck = 'sunday'
+                    downtimestocheck = downsundaytimes
+                    uptimestocheck = upsundaytimes
+                else:
+                    print("error")
+
+                datesandtimes[ formattedfuturedate + upanddown[0]] = [[formattedfuturedate,   originanddestination[0],downtimestocheck,originanddestination[1],uptimestocheck]]
+    
+            
+    pp.pprint(datesandtimes)
     return datesandtimes
+
+
+def gettingquerydata(fp):
+    """
+    This reads in route and time table information from an excel file. It also converts this excel data into a list of lists to be plugged into
+    the function gettimesdatesinfo.
+
+    Parameters:
+    fp: a string containing the filepath of the data file holding route and time info
+
+    Returns:
+    a list of list with 
+    """
+    
+    raw_data = pd.read_excel(fp)
+    
+    del raw_data['variable name']
+
+    final_list = []
+    temp_list = []
+    
+    for count, items in enumerate(raw_data):
+        
+        routesup = raw_data.iloc[0,count].split(',') 
+        routesdown = raw_data.iloc[1,count].split(',')
+    
+        routes = [routesup, routesdown]
+        temp_list.append(routes)
+
+        downweekdaytime = raw_data.iloc[2,count].split(',')
+        temp_list.append(downweekdaytime)
+        
+        downsaturdaytime = raw_data.iloc[3,count].split(',')
+        temp_list.append(downsaturdaytime)
+
+        downsundaytime = raw_data.iloc[4,count].split(',')
+        temp_list.append(downsundaytime)
+
+        upweekdaytime = raw_data.iloc[5,count].split(',')
+        temp_list.append(upweekdaytime)
+
+        upsaturdaytime = raw_data.iloc[6,count].split(',')
+        temp_list.append(upsaturdaytime)
+
+        upsundaytime = raw_data.iloc[7,count].split(',')
+        temp_list.append(upsundaytime)
+
+        final_list.append(temp_list)
+
+        temp_list = []
+
+    return final_list
+
+
 
 
 if __name__ == '__main__':
     main()
-
-
-
